96607797e7b2fa09df750355f285854a3481ab3b
rkoczur <rkoczur@gmail.com>
2024-12-06 10:41:44 +0100

    day 2 in the package

--- adventofcode2024/day2.py
+++ adventofcode2024/day2.py
@@ -1,7 +1,5 @@
 import csv
 
-result = int()
-
 def is_safe(row):
     row = [int(item) for item in row]
     row_check_dec = row.copy()
@@ -18,9 +16,21 @@ def is_safe(row):
 
     return 1
 
+def is_dapened_safe(row):
+    for i in range(len(row)):
+        removed = row.copy()
+        removed.pop(i)
+        if is_safe(removed): return 1
+    return 0
+
+result_without_dampener = int()
+result_with_dampener = int()
+
 with open('R:\\0-Források\\Scriptek\\adventofcode2024\\day2.csv') as csvfile:
     lines = csv.reader(csvfile, delimiter=' ')
     for row in lines:
-        result += is_safe(row)
+        result_without_dampener += is_safe(row)
+        result_with_dampener += is_dapened_safe(row)
 
-print(result)
+print(result_without_dampener)
+print(result_with_dampener)
