0d2fbd6b6cd94e953be432e83f3b3cc834b8e6dd
rkoczur <rkoczur@gmail.com>
2024-12-10 11:57:47 +0100

    CLEANED

--- adventofcode2024/day6.py
+++ adventofcode2024/day6.py
@@ -1,31 +1,39 @@
 
-#DECLARATION AND READ RAW DATA INTO ARRAY
+# -------------------------- #
+#         DECLARATION        #
+# -------------------------- #
+
 the_room = list()
 current_pos_x = int()
 current_pos_y = int()
+counter = 0
 
 #DIRECTIONS: 0-up, 1-right, 2-down, 3-left
 current_direction = 0
-
 
 input_data = open('adventofcode2024/day6.txt', 'r')
 lines = input_data.readlines()
 for line in lines:
     the_room.append(list(line.replace('\n','')))
 
+# -------------------------- #
+#         FUNCTIONS          #
+# -------------------------- #
+
 #RETURN THE NEXT PLACE COORDINATES ACCORDING TO DIRECTION
 def get_next_place(current_pos_x,current_pos_y):
     next_place = [current_pos_x,current_pos_y]
     if current_direction == 0:
         next_place = [current_pos_x,current_pos_y-1]
+    elif current_direction == 1:
+        next_place = [current_pos_x+1,current_pos_y]
     elif current_direction == 2:
         next_place = [current_pos_x,current_pos_y+1]
     elif current_direction == 3:
         next_place = [current_pos_x-1,current_pos_y]
-    elif current_direction == 1:
-        next_place = [current_pos_x+1,current_pos_y]
     return next_place
-        
+
+#CHANGE THE CURRENT DIRECTION
 def change_direction(current_direction):
     if current_direction<3:
         current_direction += 1
@@ -33,7 +41,7 @@ def change_direction(current_direction):
         current_direction = 0
     return current_direction
 
-#CHECK IS CURRENT POSITION IS INSIDE THE ROOM
+#CHECK IF CURRENT POSITION IS INSIDE THE ROOM
 def is_inside(x,y):
     if x >= len(the_room[0]): return False
     if x < 0: return False
@@ -48,11 +56,16 @@ for y, row in enumerate(the_room):
             current_pos_x = x
             current_pos_y = y
 
+# -------------------------- #
+#           MAIN             #
+# -------------------------- #
+
 while True:
     next_place = get_next_place(current_pos_x,current_pos_y)
     next_place_x = next_place[0]
     next_place_y = next_place[1]
     if not is_inside(next_place_x,next_place_y):
+        the_room[current_pos_y][current_pos_x] = 'X'
         break
     if the_room[next_place_y][next_place_x] == '#':
         current_direction = change_direction(current_direction)
@@ -60,8 +73,6 @@ while True:
         the_room[current_pos_y][current_pos_x] = 'X'
         current_pos_x = next_place_x
         current_pos_y = next_place_y
-
-counter = 1
 
 for row in the_room:
     for col in row:
