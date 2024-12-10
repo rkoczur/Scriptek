fd3fea7925d1aca9233d4f4ce9bbd1ca33cc60ca
rkoczur <rkoczur@gmail.com>
2024-12-10 11:51:09 +0100

    day6/1 done

--- adventofcode2024/day6.py
+++ adventofcode2024/day6.py
@@ -4,13 +4,34 @@ the_room = list()
 current_pos_x = int()
 current_pos_y = int()
 
-#DIRECTIONS: U-up, L-left, R-right, D-down
-current_direction = 'u'
+#DIRECTIONS: 0-up, 1-right, 2-down, 3-left
+current_direction = 0
+
 
 input_data = open('adventofcode2024/day6.txt', 'r')
 lines = input_data.readlines()
 for line in lines:
     the_room.append(list(line.replace('\n','')))
+
+#RETURN THE NEXT PLACE COORDINATES ACCORDING TO DIRECTION
+def get_next_place(current_pos_x,current_pos_y):
+    next_place = [current_pos_x,current_pos_y]
+    if current_direction == 0:
+        next_place = [current_pos_x,current_pos_y-1]
+    elif current_direction == 2:
+        next_place = [current_pos_x,current_pos_y+1]
+    elif current_direction == 3:
+        next_place = [current_pos_x-1,current_pos_y]
+    elif current_direction == 1:
+        next_place = [current_pos_x+1,current_pos_y]
+    return next_place
+        
+def change_direction(current_direction):
+    if current_direction<3:
+        current_direction += 1
+    else: 
+        current_direction = 0
+    return current_direction
 
 #CHECK IS CURRENT POSITION IS INSIDE THE ROOM
 def is_inside(x,y):
@@ -27,5 +48,23 @@ for y, row in enumerate(the_room):
             current_pos_x = x
             current_pos_y = y
 
-while is_inside(current_pos_x,current_pos_y):
-    
+while True:
+    next_place = get_next_place(current_pos_x,current_pos_y)
+    next_place_x = next_place[0]
+    next_place_y = next_place[1]
+    if not is_inside(next_place_x,next_place_y):
+        break
+    if the_room[next_place_y][next_place_x] == '#':
+        current_direction = change_direction(current_direction)
+    else:
+        the_room[current_pos_y][current_pos_x] = 'X'
+        current_pos_x = next_place_x
+        current_pos_y = next_place_y
+
+counter = 1
+
+for row in the_room:
+    for col in row:
+        if col == 'X': counter +=1
+
+print(counter)
